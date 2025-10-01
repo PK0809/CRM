f8ec50d72de263c1db5cefae7cad6f52d64268d7
PK0809 <nagarajnadahrts33@gmail.com>
2025-10-01 17:17:41 +0530

    Update .env.py

--- .env.py
+++ .env.py
@@ -1,4 +1,10 @@
-SECRET_KEY=your-secret-key
+USE_POSTGRES=True
 DEBUG=False
-ALLOWED_HOSTS=crm.isecuresolutions.in,www.crm.isecuresolutions.in
-DATABASE_URL=sqlite:///db.sqlite3
+SECRET_KEY=your-secret-key
+ALLOWED_HOSTS=127.0.0.1,localhost,crm.isecuresolutions.in,www.crm.isecuresolutions.in
+
+DB_NAME=crm_db
+DB_USER=pk
+DB_PASS=Nada@9945
+DB_HOST=localhost
+DB_PORT=5432
