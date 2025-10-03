658fc93597347864907a3099ac499835e8620161
PK0809 <nagarajnadahrts33@gmail.com>
2025-10-03 22:47:58 +0530

    Update .env.py
    Update .env

--- .env.py
+++ .env.py
@@ -1,17 +1,6 @@
-# Security
-SECRET_KEY=your-production-secret
-DEBUG=False
+SECRET_KEY=dy)#&kpi*)tgy=#%qu23ip7_xxk9)plw0fh7ltsv=31=u98lv!
+DEBUG=True
 USE_POSTGRES=True
-
-# Render PostgreSQL credentials
-DB_NAME=crm_db_0ndg
-DB_USER=crm_db_0ndg_user
-DB_PASS=0kbMRSTf6nFyBZyp3Zi5WDWEnlsbPpm7
-DB_HOST=dpg-d3el85l6ubrc73d0ua70-a.singapore-postgres.render.com
-DB_PORT=5432
-
-# Hosts allowed in production
-ALLOWED_HOSTS=crm.isecuresolutions.in,www.crm.isecuresolutions.in,.onrender.com,.cfargotunnel.com,127.0.0.1,localhost
-
-# Site URL for production
-SITE_URL=https://crm.isecuresolutions.in
+DATABASE_URL=postgresql://crm_db_0ndg_user:0kbMRSTf6nFyBZyp3Zi5WDWEnlsbPpm7@dpg-d3el85l6ubrc73d0ua70-a.singapore-postgres.render.com/crm_db_0ndg
+ALLOWED_HOSTS=127.0.0.1,localhost,www.crm.isecuresolutions.in,crm.isecuresolutions.in,.cfargotunnel.com,.onrender.com
+SITE_URL=http://127.0.0.1:8000
