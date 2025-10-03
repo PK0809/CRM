c88e60c8af6c446e50d40b314bc7c1f1c06c86f9
PK0809 <nagarajnadahrts33@gmail.com>
2025-10-03 23:04:59 +0530

    Delete .env.py

--- .env.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY=dy)#&kpi*)tgy=#%qu23ip7_xxk9)plw0fh7ltsv=31=u98lv!
-DEBUG=True
-USE_POSTGRES=True
-DATABASE_URL=postgresql://crm_db_0ndg_user:0kbMRSTf6nFyBZyp3Zi5WDWEnlsbPpm7@dpg-d3el85l6ubrc73d0ua70-a.singapore-postgres.render.com/crm_db_0ndg
-ALLOWED_HOSTS=127.0.0.1,localhost,www.crm.isecuresolutions.in,crm.isecuresolutions.in,.cfargotunnel.com,.onrender.com
-SITE_URL=http://127.0.0.1:8000
