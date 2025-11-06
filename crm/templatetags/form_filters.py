d5a950d95a0e83f8633450ce74a0cd4ef79ef801
PK0809 <nagarajnadahrts33@gmail.com>
2025-11-06 23:23:19 +0530

    Add files via upload

--- crm/templatetags/form_filters.py
+++ crm/templatetags/form_filters.py
@@ -1,9 +1,8 @@
-# crm/templatetags/form_filters.py
-
 from django import template
 
 register = template.Library()
 
 @register.filter(name='add_class')
-def add_class(value, css_class):
-    return value.as_widget(attrs={'class': css_class})
+def add_class(field, css):
+    """Add a CSS class to form fields dynamically."""
+    return field.as_widget(attrs={"class": css})
