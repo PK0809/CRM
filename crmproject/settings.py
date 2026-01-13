becc821bbf2037bbebbf24b9fc86faeed2f1e9b8
PK0809 <nagarajnadahrts33@gmail.com>
2026-01-14 00:38:11 +0530

    Refactor settings.py for environment variable handling

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -21,9 +21,7 @@ SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list(
     "ALLOWED_HOSTS",
-    default=[
-        "127.0.0.1",
-        "localhost",
+    CSRF_TRUSTED_ORIGINS = [
         "crm.isecuresolutions.in",
         "www.crm.isecuresolutions.in",
         "isecuresolutions.in",
@@ -120,6 +118,7 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
+
 
 
 
